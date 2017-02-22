b9f4416bee66402059731f2021689b7ed4e37887
Anastasiya Duchkina <duchkina.nast@gmail.com>
2017-02-22 18:28:36 +0200

    Fixed codestyle

--- src/AppBundle/Services/SitemapIterator.php
+++ src/AppBundle/Services/SitemapIterator.php
@@ -4,28 +4,47 @@ namespace AppBundle\Services;
 
 use Exporter\Source\SourceIteratorInterface;
 
+/**
+ * Class SitemapIterator
+ * @package AppBundle\Services
+ */
 class SitemapIterator implements SourceIteratorInterface
 {
+    /**
+     * @var integer
+     */
     protected $key;
 
+    /**
+     * @var array
+     */
     protected $current;
 
+    /**
+     * @return array
+     */
     public function current()
     {
         return $this->current;
     }
 
+    /**
+     * Method for getting next object
+     */
     public function next()
     {
         $this->key++;
-        $this->current = array(
+        $this->current = [
             'permalink'  => '/the/path/to/target',
             'lastmod'    => '',
             'changefreq' => 'weekly',
             'priority'   => 0.5
-        );
+        ];
     }
 
+    /**
+     * @return int
+     */
     public function key()
     {
         return $this->key;
@@ -39,6 +58,9 @@ class SitemapIterator implements SourceIteratorInterface
         return (null !== $this->current);
     }
 
+    /**
+     * Method for rewind
+     */
     public function rewind()
     {
         $this->key = 0;
