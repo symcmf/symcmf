0417a513ebb06c9f2943debfd73f6641513743ad
Anastasiya Duchkina <duchkina.nast@gmail.com>
2017-02-27 12:46:09 +0200

    Fixed setup.sh script

--- app/setup.sh
+++ app/setup.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-php app/console  sonata:page:create-site --enabled=true --name=SCMF --locale=- --host=localhost --relativePath=/ --enabledFrom=now --enabledTo="+10 years" --default=true
+echo 'Setup site and pages ... '
+php app/console sonata:page:create-site --enabled=true --name=SCMF --locale=- --host=localhost --relativePath=/ --enabledFrom=now --enabledTo="+10 years" --default=true
 php app/console sonata:page:update-core-routes --site=1
 php app/console sonata:page:create-snapshots --site=1
+echo ' Done! '
 
+echo 'Add admin user ...'
 php app/console fos:user:create --super-admin
+echo ' Done! '
 
+echo ' Setup styles ... '
 php app/console assetic:dump web/
+echo ' Done! '
 
 chmod 777 -R app/cache/ app/logs/
 chmod 777 -R app/cache/* app/logs/*
