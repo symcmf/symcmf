a98dd4dd9eaa803a98431636604cfaf36c72a4cf
Anastasiya Duchkina <duchkina.nast@gmail.com>
2017-03-07 10:01:48 +0200

    Added new comand in docker-install. Fixed setup.sh

--- docker-install.sh
+++ docker-install.sh
@@ -21,6 +21,9 @@ cp app/config/parameters.gitlab-ci.yml app/config/parameters.yml
 echo "Install DB and seeders for it"
 bash app/db-update.sh
 
+echo "Setup Symfony CMF"
+bash app/setup.sh
+
 echo "Generate build_bootstrap for testing"
 php vendor/sensio/distribution-bundle/Resources/bin/build_bootstrap.php
 
