e58baef81cd8a570651356ad9a6187f0c336d8df
Anastasiya Duchkina <duchkina.nast@gmail.com>
2017-03-10 15:55:05 +0200

    Fixed scrutinizer

--- initial.sh
+++ initial.sh
@@ -4,17 +4,14 @@ service mysql start
 mysql -h localhost --user=homestead --password=secret -e "CREATE DATABASE IF NOT EXISTS homestead CHARACTER SET utf8 COLLATE utf8_general_ci;"
 mysql -h localhost --user=homestead --password=secret homestead < tests/_data/dump.sql
 
-echo "Get libraries for project"
-php composer.phar install --prefer-source --no-scripts
-
 echo "Set config file with params"
 cp app/config/parameters.gitlab-ci.yml app/config/parameters.yml
 
+echo "Get libraries for project"
+composer install
+
 echo "Install DB and seeders for it"
 bash app/db-update.sh
-
-echo "Run scripts"
-php composer.phar install
 
 echo "Setup Symfony CMF"
 bash app/setup.sh
