63be92f6dbe0adbd6a5f038aebc74b5cc71cb6a5
Anastasiya Duchkina <duchkina.nast@gmail.com>
2017-03-10 16:41:44 +0200

    Fixed setup.sh

--- app/setup.sh
+++ app/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo 'Setup site and pages ... '
-php app/console sonata:page:create-site --enabled=true --name=SCMF --locale=- --host=localhost --relativePath=/ --enabledFrom=now --enabledTo="+10 years" --default=true
+php app/console sonata:page:create-site --no-confirmation=true --enabled=true --name=SCMF --locale=- --host=localhost --relativePath=/ --enabledFrom=now --enabledTo="+10 years" --default=true
 php app/console sonata:page:update-core-routes --site=1
 php app/console sonata:page:create-snapshots --site=1
 echo ' Done! '
